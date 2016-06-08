07f5e86343ec235988a5344f3bb2903223ead1ea
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-06-08 16:03:50 +0300

    docs added

--- app/components/Flights.js
+++ app/components/Flights.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Flight from '../components/Flight';
 import Header from './Header';
 
+/**
+ * Renders flight options with or without return flight pairs.
+ * @param  {Object} outFlight     Out flight.
+ * @param  {Array} returnFlights  Available return flights.
+ * @param  {Number} passengers    Number of passengers.
+ * @return {various}              Array of React components or singular component.
+ */
 function renderFlight(outFlight, returnFlights, passengers) {
   if(returnFlights)
      return returnFlights.map(returnFlight => {
+        // check return departure time for the sake of the trip
         if(returnFlight.departure > outFlight.departure)
           return (
             <Flight out={outFlight} return={returnFlight} passengers={passengers}/>
@@ -15,6 +23,11 @@ function renderFlight(outFlight, returnFlights, passengers) {
     return <Flight out={outFlight} passengers={passengers}/>;
 }
 
+/**
+ * Flights component. Renders out / return pairs of flights.
+ * @param  {Object} props Props.
+ * @return {Component}    React component.
+ */
 export default (props) => {
   const { outFlights, returnFlights, passengers } = props;
   return (
