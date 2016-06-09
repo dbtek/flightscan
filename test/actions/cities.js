e3792b9cf129d9e4f0143f0ea16d60e4fcc50069
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-06-09 09:09:41 +0300

    cities test added

---
+++ test/actions/cities.js
@@ -0,0 +1,36 @@
+import expect from 'expect';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as actions from '../../app/actions/cities';
+import * as types from '../../app/actions/types';
+import cities from '../../app/cities.json';
+
+const middlewares = [ thunk ];
+const mockStore = configureMockStore(middlewares);
+
+describe('Cities actions', () => {
+  it('creates FETCH_CITIES typed action when fetching cities has been done', () => {
+
+    const expectedActions = [
+      { type: types.REQUEST_CITIES },
+      {
+        type: types.FETCH_CITIES,
+        cities: cities
+      }
+    ]
+    const store = mockStore({
+      list: {
+        items: [],
+        isFetching: false
+      }
+    })
+
+    return store.dispatch(actions.fetchCities())
+      .then(() => { // return of async actions
+        let actions = store.getActions();
+        expect(actions[0]).toEqual(expectedActions[0]);
+        let action2 = actions[1];
+        expect(action2).toEqual(expectedActions[1]);
+      })
+  })
+})
