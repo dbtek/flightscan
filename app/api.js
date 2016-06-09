d225ab97ab350803dd66887a2e242ae56a8e0007
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-06-09 09:10:14 +0300

    env variable support added to api

--- app/api.js
+++ app/api.js
@@ -1,9 +1,15 @@
 /**
  * A resource module to simulate interaction with a REST api.
  */
-
-import FLIGHTS from './flights.json';
-import CITIES from './cities.json';
+let FLIGHTS, CITIES;
+if(process && process.env.FLIGHTS_JSON)
+  FLIGHTS = require(process.env.FLIGHTS_JSON);
+else
+  FLIGHTS = require('./flights.json');
+if(process && process.env.CITIES_JSON)
+  CITIES = require(process.env.CITIES_JSON);
+else
+  CITIES = require('./cities.json');
 
 /**
  * Fetches flights.
