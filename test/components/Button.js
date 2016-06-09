151e9c5caf8ca4bcb4e60b5190a670735d259904
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-06-09 11:12:17 +0300

    Button component tests added

---
+++ test/components/Button.js
@@ -0,0 +1,45 @@
+import expect from 'expect'
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+
+import Button from '../../app/components/Button'
+
+function setup() {
+  let props = {
+    raised: true,
+    primary: true,
+    success: true,
+    warn: true,
+    danger: true,
+    block: true,
+    onClick: expect.createSpy()
+  };
+
+  let renderer = TestUtils.createRenderer();
+  renderer.render(<Button {...props}>Test Button</Button>);
+  let output = renderer.getRenderOutput();
+
+  return {
+    props,
+    output,
+    renderer
+  };
+}
+
+describe('Button component', () => {
+  it('should render correctly', () => {
+    const { output } = setup();
+    // type
+    expect(output.type).toBe('a');
+    // children
+    expect(output.props.children).toBe('Test Button');
+    // class names
+    expect(output.props.className).toBe('button raised primary success warn danger block')
+  });
+
+  it('should call onClick when clicked', () => {
+    const { props, output } = setup();
+    output.props.onClick();
+    expect(props.onClick.calls.length).toBe(1);
+  });
+});
