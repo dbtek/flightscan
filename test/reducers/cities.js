5d6260528f601a9d906a46a209a988cd255dd491
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-06-09 11:11:48 +0300

    cities reducer tests added

---
+++ test/reducers/cities.js
@@ -0,0 +1,34 @@
+import expect from 'expect';
+import reducer from '../../app/reducers/cities';
+import * as types from '../../app/actions/types';
+
+describe('Cities reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      items: [],
+      isFetching: false
+    });
+  });
+
+  it('should handle REQUEST_CITIES', () => {
+    expect(
+      reducer([], {
+        type: types.REQUEST_CITIES
+      })
+    ).toEqual({
+      isFetching: true
+    });
+  });
+
+  it('should handle FETCH_CITIES', () => {
+    expect(
+      reducer([], {
+        type: types.FETCH_CITIES,
+        cities: ['Berlin']
+      })
+    ).toEqual({
+      items: ['Berlin'],
+      isFetching: false
+    });
+  });
+});
