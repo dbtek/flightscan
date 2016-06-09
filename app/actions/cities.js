9eff079bb6b158b52e4827276d73e24e20d8c468
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-06-09 09:08:49 +0300

    missing request action added

--- app/actions/cities.js
+++ app/actions/cities.js
@@ -3,12 +3,14 @@
  */
 
 import {
-  FETCH_CITIES
+  FETCH_CITIES,
+  REQUEST_CITIES
 } from './types';
 import * as api from '../api';
 
 export function fetchCities() {
   return async (dispatch, getState) => {
+    dispatch({type: REQUEST_CITIES});
     const results = await api.getCities();
     return dispatch({
       type: FETCH_CITIES,
