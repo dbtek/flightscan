067a8ace63a953ca0dbeedd9c193c45b65f1d3ac
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-06-09 11:12:27 +0300

    classnames added to card

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import classnames from 'classnames';
 /**
  * Card component.
  * @param  {Object} props
  * @return {Component}
  */
 export default (props) => (
-  <div className={'card ' + props.className}>
+  <div className={ classnames('card', props.className) }>
     {props.children}
   </div>
 )
