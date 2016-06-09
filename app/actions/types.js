758abfb1a06e8293de8f9e2c7e329a4c87719dc7
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-06-09 09:08:32 +0300

    missing types added

--- app/actions/types.js
+++ app/actions/types.js
@@ -2,12 +2,13 @@
  * Action types.
  */
 
-export const SEARCH_FLIGHTS        = 'SEARCH_FLIGHTS';
-export const FETCH_FLIGHTS         = 'FETCH_FLIGHTS';
-export const FETCH_CITIES          = 'FETCH_CITIES';
-export const UPDATE_FROM           = 'UPDATE_FROM';
-export const UPDATE_TO             = 'UPDATE_TO';
-export const UPDATE_DEPART_DATE    = 'UPDATE_DEPART_DATE';
-export const UPDATE_RETURN_DATE    = 'UPDATE_RETURN_DATE';
-export const UPDATE_ROUND_TRIP     = 'UPDATE_ROUND_TRIP';
-export const UPDATE_PASSENGERS     = 'UPDATE_PASSENGERS';
+export const REQUEST_FLIGHTS_SEARCH       = 'REQUEST_FLIGHTS_SEARCH';
+export const SEARCH_FLIGHTS               = 'SEARCH_FLIGHTS';
+export const FETCH_FLIGHTS                = 'FETCH_FLIGHTS';
+export const FETCH_CITIES                 = 'FETCH_CITIES';
+export const UPDATE_FROM                  = 'UPDATE_FROM';
+export const UPDATE_TO                    = 'UPDATE_TO';
+export const UPDATE_DEPART_DATE           = 'UPDATE_DEPART_DATE';
+export const UPDATE_RETURN_DATE           = 'UPDATE_RETURN_DATE';
+export const UPDATE_ROUND_TRIP            = 'UPDATE_ROUND_TRIP';
+export const UPDATE_PASSENGERS            = 'UPDATE_PASSENGERS';
