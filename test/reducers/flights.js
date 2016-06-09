0fbd022ab053af3906754a95ab085aad6ef7b0f2
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-06-09 11:12:01 +0300

    flights reducer tests added

---
+++ test/reducers/flights.js
@@ -0,0 +1,41 @@
+import expect from 'expect';
+import reducer from '../../app/reducers/flights';
+import * as types from '../../app/actions/types';
+
+describe('Flights reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      outFlights: [],
+      returnFlights: [],
+      passengers: 1,
+      isFetching: false
+    });
+  });
+
+  it('should handle REQUEST_FLIGHTS_SEARCH', () => {
+    expect(
+      reducer([], {
+        type: types.REQUEST_FLIGHTS_SEARCH
+      })
+    ).toEqual({
+      isFetching: true
+    });
+  });
+
+  it('should handle SEARCH_FLIGHTS', () => {
+    const mockFlights = [{from: '', to: ''}];
+    expect(
+      reducer([], {
+        type: types.SEARCH_FLIGHTS,
+        outFlights: mockFlights,
+        returnFlights: mockFlights,
+        passengers: 1
+      })
+    ).toEqual({
+      outFlights: mockFlights,
+      returnFlights: mockFlights,
+      isFetching: false,
+      passengers: 1
+    });
+  });
+});
